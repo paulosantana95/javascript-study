de987c64e8713e84652ae09304ce9289e6d0d926
Paulo Santana <paulosantana.sa@gmail.com>
2022-08-22 22:44:27 -0300

    Aprendendo sobre middleware

---
+++ Nodejs/12.Express Middlewares/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+// Meu middleware para aprendizado.
+const { middlewareGlobal, outroMiddleware } = require('./src/middleware/middleware');
+app.use(middlewareGlobal, outroMiddleware);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000/');
+  console.log('Server is running.');
+});
